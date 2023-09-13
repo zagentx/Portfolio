93c2c037136a46a9482b0cd6a89a673c0006320b
zagentx <solid35@web.de>
2023-09-13 12:14:53 +0200

    Revert "Fixed editing mode"
    This reverts commit 137c3bc9d7c2069b687a4f2497c65d66dbfb46a6.

--- src/app/projects/canban-board/canban-board.component.ts
+++ src/app/projects/canban-board/canban-board.component.ts
@@ -7,9 +7,6 @@ import { style } from '@angular/animations';
   styleUrls: ['./canban-board.component.scss'],
 })
 export class CanbanBoardComponent implements OnInit {
-  newTask = '';
-  isEditable = false;
-
   columns = [
     {
       title: 'To Do',
@@ -27,6 +24,7 @@ export class CanbanBoardComponent implements OnInit {
       newTask: '',
     },
   ];
+  newTask = '';
 
   ngOnInit() {
     const savedColumns = localStorage.getItem('columns');
@@ -53,9 +51,12 @@ export class CanbanBoardComponent implements OnInit {
     localStorage.setItem('columns', JSON.stringify(this.columns));
   }
 
-  toggleEdit() {
-    this.isEditable = !this.isEditable;
+  editTask(columnIndex: number, taskIndex: number, newTask: string) {
+    this.columns[columnIndex].tasks[taskIndex] = newTask;
+    this.saveTasks();
   }
+
+  tk: any;
 
   deleteTask(columnIndex: number, taskIndex: number) {
     this.columns[columnIndex].tasks.splice(taskIndex, 1);
