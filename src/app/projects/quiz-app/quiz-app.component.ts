2d61da8611bea53b10eca9667657981315ff0fea
zagentx <solid35@web.de>
2023-09-15 15:01:33 +0200

    Added questions.json

--- src/app/projects/quiz-app/quiz-app.component.ts
+++ src/app/projects/quiz-app/quiz-app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as questionsData from '../../../assets/Data/quiz-questions.json';
 
 @Component({
   selector: 'app-quiz-app',
@@ -6,12 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./quiz-app.component.scss'],
 })
 export class QuizAppComponent {
+  correctAnswer = false;
+  incorrectAnswer = false;
   title = 'Quiz-App';
   score = 0;
+  userAnswer: string = '';
   questions = [
-    { question: 'Was ist die Hauptstadt von Deutschland?', answer: 'Berlin' },
+    {
+      question: 'Was ist die Hauptstadt von Deutschland?',
+      options: ['Berlin', 'München', 'Hamburg', 'Köln'],
+      answer: 'Berlin',
+    },
     {
       question: 'Wer hat die Relativitätstheorie formuliert?',
+      options: ['Newton', 'Einstein', 'Galileo', 'Kepler'],
       answer: 'Einstein',
     },
     // Fügen Sie hier weitere Fragen hinzu...
@@ -20,7 +29,12 @@ export class QuizAppComponent {
 
   checkAnswer(userAnswer: string) {
     if (userAnswer === this.questions[this.currentQuestionIndex].answer) {
+      this.correctAnswer = true;
+
       this.score++;
+    }
+    if (userAnswer !== this.questions[this.currentQuestionIndex].answer) {
+      this.incorrectAnswer = true;
     }
     this.currentQuestionIndex++;
   }
