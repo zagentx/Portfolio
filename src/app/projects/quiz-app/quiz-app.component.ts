4c7f4e93f5977e27addf03a4a71f950191bd1c1d
zagentx <solid35@web.de>
2023-09-15 13:50:21 +0200

    Added fist quiz-app concept

--- src/app/projects/quiz-app/quiz-app.component.ts
+++ src/app/projects/quiz-app/quiz-app.component.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './quiz-app.component.html',
   styleUrls: ['./quiz-app.component.scss'],
 })
-export class QuizAppComponent {}
+export class QuizAppComponent {
+  title = 'Quiz-App';
+  score = 0;
+  questions = [
+    { question: 'Was ist die Hauptstadt von Deutschland?', answer: 'Berlin' },
+    {
+      question: 'Wer hat die Relativitätstheorie formuliert?',
+      answer: 'Einstein',
+    },
+    // Fügen Sie hier weitere Fragen hinzu...
+  ];
+  currentQuestionIndex = 0;
+
+  checkAnswer(userAnswer: string) {
+    if (userAnswer === this.questions[this.currentQuestionIndex].answer) {
+      this.score++;
+    }
+    this.currentQuestionIndex++;
+  }
+}
