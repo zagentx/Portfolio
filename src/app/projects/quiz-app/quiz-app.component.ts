83f397dc00fd68363e7e3d2958e1d9213e4bb262
zagentx ☠️ <solid35@web.de>
2023-09-16 19:43:20 +0200

    Added right & wrong icons to user answer

--- src/app/projects/quiz-app/quiz-app.component.ts
+++ src/app/projects/quiz-app/quiz-app.component.ts
@@ -60,12 +60,19 @@ export class QuizAppComponent {
   checkAnswer(userAnswer: string) {
     if (userAnswer === this.questions[this.currentQuestionIndex].answer) {
       this.correctAnswer = true;
-
+      this.incorrectAnswer = false;
       this.score++;
     }
     if (userAnswer !== this.questions[this.currentQuestionIndex].answer) {
       this.incorrectAnswer = true;
+      this.correctAnswer = false;
     }
+
+    setTimeout(() => {
+      this.correctAnswer = false;
+      this.incorrectAnswer = false;
+    }, 1000); // Setzt correctAnswer und incorrectAnswer nach 1 Sekunde (1000 Millisekunden) auf false
+
     this.currentQuestionIndex++;
   }
 
