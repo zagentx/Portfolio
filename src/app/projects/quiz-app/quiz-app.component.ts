e21e8ba7eb6ae92015ec712410f48326d34be536
zagentx ☠️ <solid35@web.de>
2023-09-17 20:35:24 +0200

    Added test json

--- src/app/projects/quiz-app/quiz-app.component.ts
+++ src/app/projects/quiz-app/quiz-app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-// import questions from '../../../assets/data/quiz-questions.json';
+// import * as quizQuestions from '../../../assets/data/quiz-questions.json';
+import * as quizData from '../../../assets/data/quiz-questions.json';
+const quizQuestions: any = (quizData as any).default;
+
 
 @Component({
   selector: 'app-quiz-app',
@@ -7,7 +10,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./quiz-app.component.scss'],
 })
 export class QuizAppComponent {
-  // Variablen als Boolesche Flags definiert
   correctAnswer = false;
   incorrectAnswer = false;
 
@@ -15,60 +17,49 @@ export class QuizAppComponent {
   score = 0;
   userAnswer: string = '';
 
-  questions = [
-    {
-      question: 'Welcher Planet ist der größte im Sonnensystem?',
-      options: ['Jupiter', 'Mars', 'Erde', 'Venus'],
-      answer: 'Jupiter',
-    },
-    {
-      question: 'Was ist die Quadratwurzel von 144?',
-      options: ['12', '14', '16', '18'],
-      answer: '12',
-    },
-    {
-      question: 'In welchem Jahr fiel die Berliner Mauer?',
-      options: ['1989', '1990', '1991', '1992'],
-      answer: '1989',
-    },
-  ];
+  questions = quizQuestions;
+
+  // questions = [
+  //   {
+  //     question: 'Welcher Planet ist der größte im Sonnensystem?',
+  //     options: ['Jupiter', 'Mars', 'Erde', 'Venus'],
+  //     answer: 'Jupiter',
+  //   },
+  //   {
+  //     question: 'Was ist die Quadratwurzel von 144?',
+  //     options: ['12', '14', '16', '18'],
+  //     answer: '12',
+  //   },
+  //   {
+  //     question: 'In welchem Jahr fiel die Berliner Mauer?',
+  //     options: ['1989', '1990', '1991', '1992'],
+  //     answer: '1989',
+  //   },
+  // ];
 
   currentQuestionIndex = 0;
 
-  // Methode hinzugefügt, um die nächste Frage im Quiz zu laden
   loadNextQuestion() {
     this.currentQuestionIndex++;
     this.userAnswer = '';
   }
 
-  // Methode checkAnswer() angepasst
   checkAnswer(userAnswer: string) {
     if (userAnswer === this.questions[this.currentQuestionIndex].answer) {
       this.correctAnswer = true;
       this.incorrectAnswer = false;
       this.score++;
-
-      // Meldung anzeigen, dass die Antwort richtig war
-      alert('Antwort richtig!');
     } else {
       this.correctAnswer = false;
       this.incorrectAnswer = true;
-
-      // Meldung anzeigen, dass die Antwort falsch war
-      alert(
-        'Antwort falsch! Die richtige Antwort ist: ' +
-          this.questions[this.currentQuestionIndex].answer,
-      );
     }
 
-    // Methode loadNextQuestion() aufgerufen, wenn die Antwort richtig oder falsch ist
     this.loadNextQuestion();
 
-    // Methode setTimeout() verwendet, um die Variablen correctAnswer und incorrectAnswer nach einer bestimmten Zeit auf false zu setzen
     setTimeout(() => {
       this.correctAnswer = false;
       this.incorrectAnswer = false;
-    }, 1000);
+    }, 2500);
   }
 
   restartQuiz() {
@@ -77,14 +68,9 @@ export class QuizAppComponent {
     this.userAnswer = '';
   }
 
-  // Funktion zum Beenden des Quiz
   endQuiz() {
-    // Quiz-Status zurücksetzen
     this.score = 0;
     this.currentQuestionIndex = 0;
     this.userAnswer = '';
-
-    // Meldung anzeigen
-    alert('Quiz beendet!');
   }
 }
