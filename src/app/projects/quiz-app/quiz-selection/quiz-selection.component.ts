7b17cea86b1b8eedd6edf00f2821f687d6b97f11
zagentx <solid35@web.de>
2023-09-18 15:19:01 +0200

    Added testsection for quiz-topic-selection

--- src/app/projects/quiz-app/quiz-selection/quiz-selection.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-quiz-selection',
-  templateUrl: './quiz-selection.component.html',
-  styleUrls: ['./quiz-selection.component.scss'],
-})
-export class QuizSelectionComponent {}
