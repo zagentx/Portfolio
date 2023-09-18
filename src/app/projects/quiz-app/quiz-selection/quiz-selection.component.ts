e53526d38023fccb495437341741c3eb748cce4f
zagentx <solid35@web.de>
2023-09-18 12:22:27 +0200

    Changed design of quizzez

---
+++ src/app/projects/quiz-app/quiz-selection/quiz-selection.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-selection',
+  templateUrl: './quiz-selection.component.html',
+  styleUrls: ['./quiz-selection.component.scss'],
+})
+export class QuizSelectionComponent {}
