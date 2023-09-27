e13cc74091ecd38e20f2e0daceef9eed3be57931
zagentx <solid35@web.de>
2023-09-27 14:21:17 +0200

    Added hover transition to projects

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  onCardClick() {
+    this.router.navigate(['/shopping-list']);
+  }
 }
